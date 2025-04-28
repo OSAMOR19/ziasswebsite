854b4539103d79bf83190441a57cb718d50c5ca6
Your Name <isaacchukwuka67@gmail.com>
2025-04-28 12:12:32 +0100

    heighrs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -204,7 +204,7 @@ export default function AboutPage() {
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
               <div className="lg:col-span-1">
-                <div className="relative h-[300px] w-full rounded-lg overflow-hidden shadow-md">
+                <div className="relative h-[400px] w-full rounded-lg overflow-hidden shadow-md">
                   <Image 
                     src={Kazem} 
                     alt="Dr. Kazeem Durodoye" 
